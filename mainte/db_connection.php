af080e6c3adc657360e8d0c9ca5ee589c77ccf20
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-09-28 19:04:17 +0900

    add メモの追加
    Signed-off-by: Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>

--- mainte/db_connection.php
+++ mainte/db_connection.php
@@ -2,5 +2,7 @@
 const DB_HOST = 'mysql:dbname=udemy_php;host=127.0.0.1';
 const DB_USER = 'php_user';
 const DB_PASSWORD = 'P@ssWord123';
+// PDOクラスのインスタンス化
+// インスタンス化したあとに、PDOの中のプロパティやメソッドにアクセスできる
 $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD);
 ?>
