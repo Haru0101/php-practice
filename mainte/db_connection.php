de5ec0af9a10fbe298cde99f99a8d5c5930efa9c
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-09-28 19:11:38 +0900

    add DB接続確認 例外処理
    Signed-off-by: Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>

--- mainte/db_connection.php
+++ mainte/db_connection.php
@@ -1,8 +1,19 @@
 <?php
-const DB_HOST = 'mysql:dbname=udemy_php;host=127.0.0.1';
+const DB_HOST = 'mysql:dbname=udemy_php;host=localhost';
 const DB_USER = 'php_user';
 const DB_PASSWORD = 'P@ssWord123';
 // PDOクラスのインスタンス化
 // インスタンス化したあとに、PDOの中のプロパティやメソッドにアクセスできる
-$pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD);
+
+// 例外処理 exception
+// 接続確認に使う
+try{
+    $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD);
+    echo '接続成功';
+}
+catch(PDOException $e) {
+    echo '接続失敗' . $e->getMessage();
+    exit();
+}
+
 ?>
